d082039cb64ce65cb4c5f3ef8ff6f8cceaf5058c
dvlaskovic <vlaskovic.dodo@gmail.com>
2026-07-04 21:36:36 +0200

    init commit

---
+++ src/main/java/com/igt/spincoreengine/SpinCoreEngineApplication.java
@@ -0,0 +1,13 @@
+package com.igt.spincoreengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinCoreEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinCoreEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igt/spincoreengine/SpinCoreEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igt.spincoreengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpinCoreEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
