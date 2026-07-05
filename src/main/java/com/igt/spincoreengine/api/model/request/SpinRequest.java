1d9b667fc7ee9e7e3b7d89f513b7ae0fea694dcf
Dobrosav <vlaskovic.dodo@gmail.com>
2026-07-05 14:41:58 +0200

    feat: add @Schema annotations to all request and response models for richer Swagger docs

--- src/main/java/com/igt/spincoreengine/api/model/request/SpinRequest.java
+++ src/main/java/com/igt/spincoreengine/api/model/request/SpinRequest.java
@@ -1,12 +1,15 @@
 package com.igt.spincoreengine.api.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
 
+@Schema(description = "Request body for playing a spin")
 public class SpinRequest implements Serializable {
+    @Schema(description = "Bet amount. Currently only 5 is accepted.", example = "5", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull(message = "Bet amount cannot be null")
     @Min(value = 5, message = "Bet amount must be equal to 5")
     @Max(value = 5, message = "Bet amount must be equal to 5")
--- src/main/java/com/igt/spincoreengine/api/model/response/BalanceResponse.java
+++ src/main/java/com/igt/spincoreengine/api/model/response/BalanceResponse.java
@@ -1,13 +1,16 @@
 package com.igt.spincoreengine.api.model.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Schema(description = "Response containing the player's current balance")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BalanceResponse implements Serializable {
 
+    @Schema(description = "Current balance of the player", example = "100.00")
     private BigDecimal balance;
 
     public BalanceResponse() {
