50fda8ee4a0f889c1569dd8e7b47e6717f509c92
dvlaskovic <vlaskovic.dodo@gmail.com>
2026-07-05 14:30:31 +0200

    feat: add AbstractIntegrationTest base class and PlayerRepository integration tests

---
+++ src/test/java/com/igt/spincoreengine/AbstractIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.igt.spincoreengine;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0");
+}
