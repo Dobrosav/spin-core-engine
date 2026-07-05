54c3ce9c1277eff806892840fea7d877a6fd62b4
dvlaskovic <vlaskovic.dodo@gmail.com>
2026-07-05 14:41:58 +0200

    feat: add @Schema annotations to all request and response models for richer Swagger docs

--- src/main/java/com/igt/spincoreengine/api/model/response/DepositResponse.java
+++ src/main/java/com/igt/spincoreengine/api/model/response/DepositResponse.java
@@ -1,13 +1,16 @@
 package com.igt.spincoreengine.api.model.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Schema(description = "Response after a successful deposit")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class DepositResponse implements Serializable {
 
+    @Schema(description = "Updated balance after the deposit", example = "150.00")
     private BigDecimal newBalance;
 
     public DepositResponse() {
